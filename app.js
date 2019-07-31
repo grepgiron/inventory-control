aeb66f25dc9228fe976ce47c49097bd11f9f3708
grepgiron <gregoriopaguada@outlook.com>
2019-07-31 11:13:57 -0600

    second commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+
+//-----Connecting Database-------
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/inventory_management', { useNewUrlParser: true })
+	.then(() => { 
+		console.log('Database Connect') 
+		},
+		error => {
+			console.log('Database could not be connect: ' + error)
+		});
+
+//-----Config Express------------
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended:false
+}));
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
